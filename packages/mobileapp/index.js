2ac7dd791812caa8023d40b3fa0c613aa6199891
Himanshu Tomar <eternal_mangekyo@Himanshus-MacBook-Pro.local>
2019-06-13 21:10:41 +0530

    Working Imports

--- packages/mobileapp/index.js
+++ packages/mobileapp/index.js
@@ -1,8 +1,9 @@
-import { KeepAwake, registerRootComponent } from 'expo';
-import App from './App';
+/**
+ * @format
+ */
 
-if (__DEV__) {
-  KeepAwake.activate();
-}
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
 
-registerRootComponent(App);
+AppRegistry.registerComponent(appName, () => App);
